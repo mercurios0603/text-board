47a896985c7ab1167df53075b76be5574cd7c5ea
mercurios0603 <mercurios0603@gmail.com>
2023-09-11 14:59:55 +0900

    text board proto release v1

--- main/java/Main.java
+++ main/java/Main.java
@@ -17,13 +17,17 @@ public class Main {
         // 코드 정렬 (심심할 때마다 눌러주세요)
         // Alt + Ctrl + L
 
+        ArrayList<Integer> number = new ArrayList<>();
         ArrayList<Object> subject = new ArrayList<>();
         ArrayList<Object> detail = new ArrayList<>();
 
+        int listnumber = 0; // 글작성 번호는 프로그램이 살아있는 한 계속해서 증가되어야 한다.
+
         while (true) { // 무한 반복문
-            System.out.println("메뉴 입력 : ");
+            System.out.print("메뉴 입력 : ");
             Scanner scan = new Scanner(System.in);
             String func = scan.next();
+
 
             if (func.equals("a")) {
                 System.out.println("기능1");
@@ -33,29 +37,34 @@ public class Main {
                 System.out.println("a = 기능1");
                 System.out.println("b = 기능2");
             } else if (func.equals("add")) {
+
                 // 공백을 포함한 한 문장을 가져오는 방법
                 System.out.println("게시물을 작성합니다.\n");
 
                 scan.nextLine(); // 버퍼 비우기
 
-                System.out.println("게시물 제목을 입력해주세요 : ");
+                System.out.print("게시물 제목을 입력해주세요 : ");
                 String title = scan.nextLine();
                 System.out.println("당신이 입력한 제목은 : " + title);
 
-                System.out.println("게시물 내용을 입력해주세요 : ");
+                System.out.print("게시물 내용을 입력해주세요 : ");
                 String contents = scan.nextLine();
                 System.out.println("당신이 입력한 내용은 : " + contents);
 
-                subject.add(title);
-                detail.add(contents);
 
-                System.out.println("게시물이 등록되었습니다.");
+                listnumber++; // listnumber 값을 1 증가
+
+                number.add(listnumber); // 현재의 listnumber 값을 number 배열에 저장
+                subject.add(title); // 입력한 제목을 subject 배열에 저장
+                detail.add(contents); // 입력한 내용을 detail 배열에 저장
+
+                System.out.println(listnumber + "번 게시물이 등록되었습니다.");
 
             } else if (func.equals("list")) {
                 System.out.println("===============================");
                 for (int i = 0; i < subject.size(); i++) {
-                    System.out.println((i+1) + "번 게시물의 내용은 다음과 같습니다.");
-                    System.out.println("번호 : " + (i+1));
+                    System.out.println(number.get(i) + "번 게시물의 내용은 다음과 같습니다.");
+                    System.out.println("번호 : " + number.get(i));
                     System.out.println("제목 : " + subject.get(i));
                     System.out.println("내용 : " + detail.get(i));
                     System.out.println("===============================");
@@ -64,27 +73,65 @@ public class Main {
 
                 scan.nextLine(); // 버퍼 비우기
 
-                System.out.println("수정할 게시물 번호를 입력해주세요 : ");
-                String inputidx = scan.nextLine();
-                int postidx = Integer.parseInt(inputidx) - 1;
+                System.out.print("수정할 게시물 번호를 입력해주세요 : ");
+                String inputidx = scan.next();
+                int postidx = Integer.parseInt(inputidx);
 
-                if (postidx < subject.size()) {
+                // indexOf 메서드를 사용하여 해당 게시물 번호를 가지는 인덱스 찾기
+                int index = number.indexOf(postidx);
 
-                    System.out.println("수정할 제목을 입력해주세요 : ");
-                    String modifytitle = scan.nextLine();
-                    System.out.println("당신이 수정한 제목은 : " + modifytitle);
+                // 만약 postidx가 특정 값을 가지면 (존재한다면 index에 그 값이 들어갈 것이고, 아니면 0을 반환)
 
-                    System.out.println("수정할 내용을 입력해주세요 : ");
-                    String modifycontents = scan.nextLine();
-                    System.out.println("당신이 수정한 내용은 : " + modifycontents);
+                    if (index != -1) {
 
-                    subject.set(postidx, modifytitle);
-                    detail.set(postidx, modifycontents);
+                        scan.nextLine(); // 버퍼 비우기
 
-                    System.out.println("게시물이 수정되었습니다.");
+                        System.out.print("수정할 제목을 입력해주세요 : ");
+                        String modifytitle = scan.nextLine();
+                        System.out.println("당신이 수정한 제목은 : " + modifytitle);
+
+                        System.out.print("수정할 내용을 입력해주세요 : ");
+                        String modifycontents = scan.nextLine();
+                        System.out.println("당신이 수정한 내용은 : " + modifycontents);
+
+                        subject.set(index, modifytitle);
+                        detail.set(index, modifycontents);
+
+                        System.out.println("게시물이 수정되었습니다.");
+
+                    } else {
+
+                        System.out.println("없는 게시물 번호입니다.");
+
+                    }
+
+
+
+
+            } else if (func.equals("delete")) {
+
+                scan.nextLine(); // 버퍼 비우기
+
+                System.out.print("삭제할 게시물 번호를 입력해주세요 : ");
+
+                String inputidx2 = scan.nextLine();
+                int postidx2 = Integer.parseInt(inputidx2);
+
+                // indexOf 메서드를 사용하여 해당 게시물 번호를 가지는 인덱스 찾기
+                int index2 = number.indexOf(postidx2);
+
+                if (index2 != -1) {
+
+                    number.remove(index2);
+                    subject.remove(index2);
+                    detail.remove(index2);
+
+                    System.out.println("게시물이 삭제되었습니다.");
+
                 } else {
                     System.out.println("없는 게시물 번호입니다.");
                 }
+
 
             } else if (func.equals("exit")) {
                 System.out.println("프로그램을 종료합니다.");
